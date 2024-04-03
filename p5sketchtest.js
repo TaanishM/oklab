bba10be8291afdf51cb6874ef3e4a7c57f5c3ebb
Akashi <161021770+TaanishM@users.noreply.github.com>
2024-04-03 10:19:08 +0530

    comments added

--- p5sketchtest.js
+++ p5sketchtest.js
@@ -2,8 +2,8 @@ import Sketch from "react-p5";
 import React,{useState} from "react";
 
 const P5Sketch = (props) => {
-  const {data,clickstat} = props;
-  const [particles, setParticles] = useState(
+  const {data,clickstat} = props;                         //props from main component
+  const [particles, setParticles] = useState(             //particle state init
 
     Array.from({ length: 1000 }, () => ({
 
@@ -16,26 +16,26 @@ const P5Sketch = (props) => {
     }))
 
   );
-// let particles = [];
-const num = 100;
-const scaleFactor = 0.001;
-const speed = 0.4;
 
-const [slider, setSlider] = useState(null);
+const num = 100;                                            //number of particles on screen
+const scaleFactor = 0.001;                                  //noise scaling
+const speed = 0.4;                                          //base speed
 
-const setup = (p5,canvasParentRef) => {
+const [slider, setSlider] = useState(null);                 //slider state init
+
+const setup = (p5,canvasParentRef) => {                     //canvas setup
   p5.createCanvas(578, 600).parent(canvasParentRef);
 
-  p5.stroke(100);
-  p5.colorMode(p5.RGB);
-  for (let i = 0; i < num; i++) {
+  p5.stroke(100); //colormode seed
+  p5.colorMode(p5.RGB); //colormode select
+  for (let i = 0; i < num; i++) {                           //initial position seed
     let x = p5.random(p5.width);
     let y = p5.random(p5.height);
     let z = p5.random(5);
     particles[i] = { x, y, z };
   }
       
-  const localSlider = p5.createSlider(0, 32, 1, 1);
+  const localSlider = p5.createSlider(0, 32, 1, 1);         //slider initialisation
   localSlider.position(1360, 157);
   localSlider.size(100);
   localSlider.style("transform", "rotate(270deg)");
@@ -48,25 +48,23 @@ const setup = (p5,canvasParentRef) => {
 }
 
 const draw = (p5)=> {
-  let g = slider.value();
-  for (let i = 0; i < num; i++) {
+  let g = slider.value();                                   //slider value storage
+
+  for (let i = 0; i < num; i++) {                           //particle draw
     let p = particles[i];
     p5.strokeWeight(p.z);
     p5.point(p.x, p.y);
 
-    let n = p5.noise(p.x * scaleFactor, p.y * scaleFactor);
-    // TAU is 2 * PI
-    // multiply by 2 to prevent perlin bias
-    let a = g * 2 * 3.14 * n;
+    let n = p5.noise(p.x * scaleFactor, p.y * scaleFactor); //perlin noise seed
+  
+    let a = g * 2 * 3.14 * n;                               //noise scaler linked to slider
 
-    // bias to the left as perlin clumps around 0.5
-    p.x += Math.cos(a) * speed;
+    p.x += Math.cos(a) * speed;                             //left bias to prevent clumpin
     p.y += Math.sin(a) * speed;
     
    
-    p5.stroke(data[0][0],data[0][1],data[0][2]);
+    p5.stroke(data[0][0],data[0][1],data[0][2]);            //colour seed
     p5.point(p.x+10,p.y+10);
-    
     p5.stroke(data[1][0],data[1][1],data[1][2]);
     p5.point(p.x+20,p.y+20);
     p5.stroke(data[2][0],data[2][1],data[2][2]);
@@ -77,11 +75,10 @@ const draw = (p5)=> {
     p5.point(p.x+50,p.y+50);
     p5.stroke(data[5][0],data[5][1],data[5][2]);
 
-    // changes bias to left/right up/down
-    p.x += Math.cos(a) * (i % 2 ? 1 : -1);
+    p.x += Math.cos(a) * (i % 2 ? 1 : -1);                  //shift bias to left/right up/down
     p.y += Math.sin(a) * (i % 2 ? 1 : -1);
 
-    if (!onScreen({ x: p.x, y: p.y }, p5)) {
+    if (!onScreen({ x: p.x, y: p.y }, p5)) {                //re-render trigger
 
       p.x = p5.random(p5.width);
 
@@ -91,24 +88,25 @@ const draw = (p5)=> {
     
   }
 }
-function onScreen(v,p5) {
+
+function onScreen(v,p5) {                                   //checks if particles reach end of screen              
     return v.x >= 0 && v.x <= p5.width && v.y >= 0 && v.y <= p5.height;
   }
   
-  function mp (p5){
-    if(clickstat==="true"){
+  function mp (p5){                                         
+    if(clickstat==="true"){                                 //randomise/refresh button is pressed   
     p5.clear();
     p5.noiseSeed(p5.millis());
     p5.redraw(); 
   }
-  if(clickstat==="snap"){
+  if(clickstat==="snap"){                                   //check if screenshot is pressed
     p5.noLoop();
     p5.saveCanvas();
     setTimeout(() => {
       p5.loop()}, 500);
   }
 }
-  return (
+  return (                                                  //p5 component call
     <Sketch setup={setup} draw={draw} data={data} mousePressed={p5 => mp(p5)}/>
 )  
 }
