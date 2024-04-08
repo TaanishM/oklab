302004b5a7bff04cfa39bd6848bb0bb5ea6c0ead
Akashi <161021770+TaanishM@users.noreply.github.com>
2024-04-08 11:17:30 +0530

    corrections

--- colourcorrect.js
+++ colourcorrect.js
@@ -1,4 +1,4 @@
-import React, { useState,useRef } from "react";
+import React, { useState } from "react";
 import "./pengine.css";
 // import Randcolor from "./colorselector";
 import P5Sketch from "./p5perlin";
